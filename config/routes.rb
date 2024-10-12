96e34c96cfd3be47504fba1602c5024efbdff699
Gabriel Fiorese <gsfiorese@gmail.com>
2024-10-12 14:38:06 +1100

    fancy done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # get "tasks" => "tasks#index"
+  # get "tasks/:id" => "tasks#show"
+  resources :tasks
 end
